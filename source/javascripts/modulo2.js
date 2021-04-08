31f1f4b4b3ad530337d9bc3647562d14f7b57200
Amanda Orbe <amandaorbe@gmail.com>
2021-04-08 12:25:35 +0200

    sidebar tema2

--- source/javascripts/modulo2.js
+++ source/javascripts/modulo2.js
@@ -5,47 +5,29 @@
 
 
 
-const oncogenesisPresentacionTab = document.getElementById("presentacion-side");
-const oncogenesisPresentacion = document.getElementById("oncogenesis-presentacion");
+const profilaxisPresentacionTab = document.getElementById("profilaxis-presentacion-side");
+const profilaxisPresentacion = document.getElementById("profilaxis-presentacion");
 
-const oncogenesisObjetivosTab = document.getElementById("objetivos-side");
-const oncogenesisObjetivos = document.getElementById("oncogenesis-objetivos");
+const profilaxisObjetivosTab = document.getElementById("profilaxis-objetivos-side");
+const profilaxisObjetivos = document.getElementById("profilaxis-objetivos");
 
-const introduccionTab = document.getElementById("introduccion-side");
-const introduccion = document.getElementById("oncogenesis-introduccion");
+const profilaxisIngresadoTab = document.getElementById("profilaxis-ingresado-side");
+const profilaxisIngresado = document.getElementById("profilaxis-ingresado");
 
-const mantenimientoTab = document.getElementById("mantenimiento-side");
-const mantenimiento = document.getElementById("oncogenesis-mantenimiento");
+const profilaxisQuirurgicoTab = document.getElementById("profilaxis-quirurgico-side");
+const profilaxisQuirurgico = document.getElementById("profilaxis-quirurgico");
 
-const perdidaTab = document.getElementById("perdida-side");
-const perdida = document.getElementById("oncogenesis-perdida");
+const profilaxisCateterTab = document.getElementById("profilaxis-cateter-side");
+const profilaxisCateter = document.getElementById("profilaxis-cateter");
 
-const resistenciaTab = document.getElementById("resistencia-side");
-const resistencia = document.getElementById("oncogenesis-resistencia");
+const profilaxisAmbulatorioTab = document.getElementById("profilaxis-ambulatorio-side");
+const profilaxisAmbulatorio = document.getElementById("profilaxis-ambulatorio");
 
-const activacionTab = document.getElementById("activacion-side");
-const activacion = document.getElementById("oncogenesis-activacion");
+const profilaxisEstudiosTab = document.getElementById("profilaxis-estudios-side");
+const profilaxisEstudios = document.getElementById("profilaxis-estudios");
 
-const induccionTab = document.getElementById("induccion-side");
-const induccion = document.getElementById("oncogenesis-induccion");
-
-const invasionTab = document.getElementById("invasion-side");
-const invasion = document.getElementById("oncogenesis-invasion");
-
-const inestabilidadTab = document.getElementById("inestabilidad-side");
-const inestabilidad = document.getElementById("oncogenesis-inestabilidad");
-
-const inflamacionTab = document.getElementById("inflamacion-side");
-const inflamacion = document.getElementById("oncogenesis-inflamacion");
-
-const alteracionTab = document.getElementById("alteracion-side");
-const alteracion = document.getElementById("oncogenesis-alteracion");
-
-const evasionTab = document.getElementById("evasion-side");
-const evasion = document.getElementById("oncogenesis-evasion");
-
-const oncogenesisBiblioTab = document.getElementById("biblio-side");
-const oncogenesisBiblio = document.getElementById("oncogenesis-biblio");
+const profilaxisAnticoagulantesTab = document.getElementById("profilaxis-anticoagulantes-side");
+const profilaxisAnticoagulantes = document.getElementById("profilaxis-anticoagulantes");
 
 
 
@@ -55,21 +37,22 @@ const oncogenesisBiblio = document.getElementById("oncogenesis-biblio");
 
 
 
-const oncogenesisChapters = [{tab: oncogenesisPresentacionTab, content: oncogenesisPresentacion} ,
-                  {tab: oncogenesisObjetivosTab, content: oncogenesisObjetivos} ,
-                  {tab: introduccionTab, content: introduccion},
-                  {tab: mantenimientoTab, content: mantenimiento},
-                  {tab: perdidaTab, content: perdida},
-                  {tab: resistenciaTab, content: resistencia},
-                  {tab: activacionTab, content: activacion},
-                  {tab: induccionTab, content: induccion},
-                  {tab: invasionTab, content: invasion},
-                  {tab: inestabilidadTab, content: inestabilidad},
-                  {tab: inflamacionTab, content: inflamacion},
-                  {tab: alteracionTab, content: alteracion},
-                  {tab: evasionTab, content: evasion},
-                  // {tab: oncogenesisBiblioTab, content: oncogenesisBiblio},
+
+
+const profilaxisChapters = [{tab: profilaxisPresentacionTab, content: profilaxisPresentacion} ,
+                  {tab: profilaxisObjetivosTab, content: profilaxisObjetivos} ,
+                  {tab: profilaxisIngresadoTab, content: profilaxisIngresado},
+                  {tab: profilaxisQuirurgicoTab, content: profilaxisQuirurgico},
+                  {tab: profilaxisCateterTab, content: profilaxisCateter},
+                  {tab: profilaxisAmbulatorioTab, content: profilaxisAmbulatorio}
                 ]
+
+
+
+const profilaxisSubChapters = [{tab: profilaxisEstudiosTab, content: profilaxisEstudios} ,
+                  {tab: profilaxisAnticoagulantesTab, content: profilaxisAnticoagulantes} ,
+                ]
+
 
 
 
@@ -87,7 +70,7 @@ const oncogenesisChapters = [{tab: oncogenesisPresentacionTab, content: oncogene
 
 
 window.addEventListener('scroll', function(){
-  oncogenesisChapters.forEach(function(chapter){
+  profilaxisChapters.forEach(function(chapter){
 
 
     if ((chapter.content.offsetTop - 500) < (window.scrollY ) && (window.scrollY ) < ((chapter.content.offsetTop- 500 ) + chapter.content.scrollHeight)) {
@@ -108,6 +91,26 @@ window.addEventListener('scroll', function(){
 });
 
 
+window.addEventListener('scroll', function(){
+  profilaxisSubChapters.forEach(function(chapter){
+
+
+    if ((chapter.content.offsetTop - 500) < (window.scrollY ) && (window.scrollY ) < ((chapter.content.offsetTop- 500 ) + chapter.content.scrollHeight)) {
+      chapter.tab.classList.add('sidebar-content__item--current');        
+      chapter.tab.classList.remove('sidebar__content__read');
+
+      } else if ((window.scrollY ) >= (chapter.content.offsetTop - 500  + chapter.content.scrollHeight )){
+        chapter.tab.classList.remove('sidebar-content__item--current');
+        chapter.tab.classList.add('sidebar__content__read');
+        // console.log(chapter.tab.classList + "is" + chapter.content.offsetTop  )
+      } else if (chapter.tab.classList.contains('sidebar-content__item--current') && (chapter.content.offsetTop - 500 > (window.scrollY )))  {
+        chapter.tab.classList.remove('sidebar-content__item--current');
+        // chapter.tab.classList.remove('sidebar__content__read');
+      }
+    });
+
+});
+
 // // **************************** LOCAL STORAGE*****************************
 
 
@@ -118,11 +121,10 @@ window.addEventListener('scroll', function(){
 
 //////////
 
-const oncogenesis = document.getElementById('oncogenesis');
+const profilaxis = document.getElementById('profilaxis');
 
 
-  oncogenesis.addEventListener("click", function(event){
-    console.log("oncogenesis")
+  profilaxis.addEventListener("click", function(event){
     event.currentTarget.parentNode.classList.toggle("sidebar-content__invisible");
     event.currentTarget.querySelector(".fa-chevron-down").classList.toggle("sidebar-content__chevron-up");
 
@@ -131,11 +133,11 @@ const oncogenesis = document.getElementById('oncogenesis');
 // ************************************ PHONE MENU ****************************
 
 
-const oncogenesisPhoneButton = document.getElementById("oncogenesis-navigation__button");
-const oncogenesisSidebarItems = document.querySelectorAll(".sidebar-content__chapter, .sidebar-content__subchapter") ;
+const profilaxisPhoneButton = document.getElementById("profilaxis-navigation__button");
+const profilaxisSidebarItems = document.querySelectorAll(".sidebar-content__chapter, .sidebar-content__subchapter") ;
 
 
-oncogenesisPhoneButton.addEventListener("click", function(event){
+profilaxisPhoneButton.addEventListener("click", function(event){
   document.getElementById("sidebar").classList.toggle("width-zero");
   document.getElementById("main").classList.toggle("width-zero");
   document.getElementById("main").classList.toggle("hidden-phone-main");
@@ -146,7 +148,7 @@ oncogenesisPhoneButton.addEventListener("click", function(event){
 });
 
 
-oncogenesisSidebarItems.forEach(function(item){
+profilaxisSidebarItems.forEach(function(item){
   item.addEventListener("click", function(event){
     console.log(item);
     document.getElementById("sidebar").classList.add("width-zero");
